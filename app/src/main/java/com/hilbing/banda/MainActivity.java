030eb6bd688d88067e406f6281473e58e34e7284
ghilbing <ghilbing@gmail.com>
2019-11-04 15:55:14 -0800

    Navigation drawer with fragments working

--- app/src/main/java/com/hilbing/banda/MainActivity.java
+++ app/src/main/java/com/hilbing/banda/MainActivity.java
@@ -1,23 +1,37 @@
 package com.hilbing.banda;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
+import android.view.MenuItem;
+import android.widget.Toast;
 
-import com.google.firebase.auth.FirebaseAuth;
-import com.hilbing.banda.ui.SignInActivity;
+import com.google.android.material.navigation.NavigationView;
+import com.hilbing.banda.fragment.ConcertFragment;
+import com.hilbing.banda.fragment.PlaylistFragment;
+import com.hilbing.banda.fragment.RehearsalFragment;
+import com.hilbing.banda.fragment.SettingFragment;
+import com.hilbing.banda.fragment.UserBandaFragment;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
 
-    @BindView(R.id.logout)
-    Button logout;
-    private FirebaseAuth.AuthStateListener mAuthStateListener;
+    @BindView(R.id.toolbar)
+    Toolbar toolbar;
+
+    @BindView(R.id.drawer_layout)
+    DrawerLayout drawer;
+
+    @BindView(R.id.nav_view)
+    NavigationView navigationView;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,13 +40,62 @@ public class MainActivity extends AppCompatActivity {
 
         ButterKnife.bind(this);
 
-        logout.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                FirebaseAuth.getInstance().signOut();
-                Intent intent = new Intent(MainActivity.this, SignInActivity.class);
-                startActivity(intent);
-            }
-        });
+        setSupportActionBar(toolbar);
+
+        navigationView.setNavigationItemSelectedListener(this);
+
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+
+        if (savedInstanceState == null){
+            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new UserBandaFragment()).commit();
+            navigationView.setCheckedItem(R.id.nav_perfil);
+        }
+
+
+    }
+
+
+
+    @Override
+    public void onBackPressed() {
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+
+            super.onBackPressed();
+        }
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+        switch (menuItem.getItemId()){
+            case R.id.nav_perfil:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new UserBandaFragment()).commit();
+                break;
+            case R.id.nav_playlist:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new PlaylistFragment()).commit();
+                break;
+            case R.id.nav_rehearsal:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new RehearsalFragment()).commit();
+                break;
+            case R.id.nav_concert:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new ConcertFragment()).commit();
+                break;
+            case R.id.nav_settings:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new SettingFragment()).commit();
+                break;
+            case R.id.nav_share:
+                Toast.makeText(this, getResources().getString(R.string.share), Toast.LENGTH_LONG).show();
+                break;
+            case R.id.nav_send:
+                Toast.makeText(this, getResources().getString(R.string.send), Toast.LENGTH_LONG).show();
+                break;
+
+        }
+
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
     }
 }
